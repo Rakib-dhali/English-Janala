915dfab178076161f9e743f1b3e8b45afcbb9680
Rakib-dhali~ <rakibdhai343@gmail.com>
2026-03-04 12:03:27 +0600

    Load Words by Level and Display Cards

--- script/index.js
+++ script/index.js
@@ -3,6 +3,27 @@ const loadLessons = () => {
     .then((response) => response.json()) //promise of json data
     .then((json) => displayLesson(json.data));
 };
+
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url).then(res => res.json()).then(data => displayLevelWord(data.data))
+}
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container")
+    wordContainer.innerHTML=""
+
+    words.forEach(word => {
+        console.log(word);
+        const card = document.createElement('div')
+        card.innerHTML=`<p>cat<p>`
+
+        wordContainer.append(card)
+        
+    });
+
+}
+
 const displayLesson = (lessons) => {
   const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
@@ -10,7 +31,7 @@ const displayLesson = (lessons) => {
     console.log(lesson);
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-    <button class="border-2 rounded-md border-[#422AD5]">
+    <button onclick="loadLevelWord(${lesson.level_no})" class="border-2 rounded-md border-[#422AD5]">
         <a class="flex gap-2 px-4 py-2">
         <img src="./assets/fa-book-open.png" alt="" />
         <p class="text-[#422AD5] text-sm leading-5 font-semibold">lesson-${lesson.level_no}</p>
