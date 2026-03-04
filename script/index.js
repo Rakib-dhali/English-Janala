a1c851eef7a01a5508e4d3e4d9ea82e2352fa342
Rakib-dhali~ <rakibdhai343@gmail.com>
2026-03-04 13:18:22 +0600

    Handle Errors , empty lessons for Better UX

--- script/index.js
+++ script/index.js
@@ -5,18 +5,34 @@ const loadLessons = () => {
 };
 
 const loadLevelWord = (id) => {
-    const url = `https://openapi.programming-hero.com/api/level/${id}`
-    fetch(url).then(res => res.json()).then(data => displayLevelWord(data.data))
-}
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
 
 const displayLevelWord = (words) => {
-    const wordContainer = document.getElementById("word-container")
-    wordContainer.innerHTML=""
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
 
-    words.forEach(word => {
-        console.log(word);
-        const card = document.createElement('div')
-        card.innerHTML=`
+  if (words.length === 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center  col-span-full rounded-xl py-10 space-y-6 font-bangla">
+        <img class="mx-auto" src="./assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-400">
+          এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+    </div>
+    
+    `;
+    return;
+  }
+
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
         <div class="bg-white rounded text-center shadow-sm py-10 px-5 spacey-y-4">
         <h2 class="font-bold text-2xl">${word.word}</h2>
         <p class="font-semibold text-2xl">Meaning/Pronounciation</p>
@@ -28,13 +44,11 @@ const displayLevelWord = (words) => {
           <button class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
 
-      </div>`
+      </div>`;
 
-        wordContainer.append(card)
-        
-    });
-
-}
+    wordContainer.append(card);
+  });
+};
 
 const displayLesson = (lessons) => {
   const levelContainer = document.getElementById("level-container");
@@ -51,7 +65,7 @@ const displayLesson = (lessons) => {
     </button>
         `;
 
-    levelContainer.append(btnDiv)
+    levelContainer.append(btnDiv);
   }
 };
 loadLessons();
